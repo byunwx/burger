a68cd97d17299d97aee6f4cff862abcef4729ed0
Woojoo Byun <woojoobyun@Woojoos-MacBook-Pro.local>
2018-03-19 17:13:34 -0400

    connection

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,11 @@
 const mysql = require('mysql');
+var connection;
 if (process.env.JAWSDB_URL) {
   connection=mysql.createConnection(process.env.JAWSDB_URL);
+
 }else{
-  const connection = mysql.createConnection({
+
+  connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
@@ -12,7 +15,7 @@ if (process.env.JAWSDB_URL) {
 }
 
 
-connection.connect((err) =>{
+connection.connect(function(err){
 if (err) throw err;
 console.log('connected as id ' + connection.threadId);
 });
